ed9d3cd29276c07ef4f3c2cf496392ca0a9daeeb
IEVN1003-22002251 <78992@alumnos.utleon.edu.mx>
2025-09-17 19:42:19 -0600

    Formularios

--- src/app/heroes/heroes-list/heroes-list.component.ts
+++ src/app/heroes/heroes-list/heroes-list.component.ts
@@ -4,8 +4,48 @@ import { Component } from '@angular/core';
   selector: 'app-heroes-list',
   standalone: false,
   templateUrl: './heroes-list.component.html',
-  styleUrl: './heroes-list.component.css'
+
+
 })
 export class HeroesListComponent {
+  imageWidth:Number=40;
+  imageMargin:Number=2;
+  muestraImage:boolean=true;
+  listFilter:string='';
 
+
+  showImage():void{
+    this.muestraImage=!this.muestraImage;
+  }
+
+  heroes:any[]=[
+    {
+      imagen:"https://dragonball-api.com/characters/goku_normal.webp",
+      nombre:"Goku",
+      description:"Kame Hame Ha",
+      race:"Saiyan",
+      ki:9000
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/vegeta_normal.webp",
+      nombre:"Vegeta",
+      description:"Final Flash",
+      race:"Saiyan",
+      ki:8500
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/picolo_normal.webp",
+      nombre:"Piccolo",
+      description:"Makankosapo",
+      race:"Namek",
+      ki:7000
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/Freezer.webp",
+      nombre:"Freezer",
+      description:"Death Ball",
+      race:"Alien",
+      ki:9500
+    }
+  ]
 }
